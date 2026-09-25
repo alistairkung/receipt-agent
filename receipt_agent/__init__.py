04187698448f9c71be60da9bb9f963cfb196defb
alistairkung <alistaircckung@gmail.com>
2026-09-25 17:55:38 +0800

    Add standalone receipt agent package

---
+++ receipt_agent/__init__.py
@@ -0,0 +1,8 @@
+"""Standalone receipt-agent package.
+
+Adapted from the author's FTEC5660 homework implementation.
+"""
+
+from .pipeline import ReceiptPipeline, ReceiptRunResult
+
+__all__ = ["ReceiptPipeline", "ReceiptRunResult"]
